f180f669b12ac31772a9f3256b62e9bee6d51b63
Wert5 <grenier.winston@gmail.com>
2016-11-12 17:07:12 -0500

    add ServoTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+        import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+        import com.qualcomm.robotcore.hardware.DcMotor;
+        import com.qualcomm.robotcore.hardware.Servo;
+        import com.qualcomm.robotcore.util.Range;
+@TeleOp(name="ServoZipTest", group="ServoZipTest")
+public class ServoTest extends OpMode {
+    
+    final static double ZIP_IN_L = 0.0;
+    
+    final static double ZIP_OUT_L = 1.0;
+        // position of the zip servo.
+    double zipPosL;
+    
+    Servo zipL;
+    
+
+    public ServoTest() {}
+
+    
+
+    public void init() {
+        zipL = hardwareMap.servo.get("zipL");         // assign the starting position of the wrist and dump
+        zipPosL = ZIP_IN_L;
+    }
+
+    @Override
+
+    public void loop() {
+        if (gamepad2.dpad_down) {
+            zipPosL = ZIP_IN_L;
+        } else if (gamepad2.dpad_left) {
+            zipPosL = ZIP_OUT_L;
+        } else if (gamepad2.dpad_right) {
+            zipPosL = ZIP_IN_L;
+        }
+        zipL.setPosition(zipPosL);
+        telemetry.addData("zipL", "zipL:  " + String.format("%.2f", zipPosL));
+    }
+}
